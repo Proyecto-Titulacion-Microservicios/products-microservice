32350c5dc08f344010a919f248ce275c1bf5cb44
Ricardo Tipan <ricardojaviertipan@gmail.com>
2025-03-08 20:00:01 -0500

    configuracion de validaciones

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,1 +1,12 @@
-export class CreateProductDto {}
+import { IsNumber, IsString, Min } from "class-validator";
+
+export class CreateProductDto {
+
+    @IsString()
+    public name: string;
+    @IsNumber({
+        maxDecimalPlaces: 4,
+    })
+    @Min(0)
+    public price: string;
+}
